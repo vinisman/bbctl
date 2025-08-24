b7e972f7070550110700ae05ce52b522fac855cb
vinisman <vinisman@mail.ru>
2025-08-24 19:48:09 +0300

    Change keys for get command

--- cmd/repo/required-build/get.go
+++ cmd/repo/required-build/get.go
@@ -3,6 +3,7 @@ package requiredbuild
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/vinisman/bbctl/internal/bitbucket"
@@ -11,22 +12,29 @@ import (
 
 func GetRequiredBuildCmd() *cobra.Command {
 	var (
-		projectKey string
-		slug       string
-		output     string
+		repositorySlug string
+		output         string
 	)
 	cmd := &cobra.Command{
 		Use:   "get",
 		Short: "Get list of required-builds for repositories",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if projectKey == "" || slug == "" {
-				return fmt.Errorf("please specify --project and --slug")
+			if repositorySlug == "" {
+				return fmt.Errorf("please specify --repositorySlug")
 			}
 
 			client, err := bitbucket.NewClient(context.Background())
 			if err != nil {
 				return err
 			}
+
+			parts := strings.SplitN(repositorySlug, "/", 2)
+			if len(parts) != 2 || parts[1] == "" {
+				client.Logger.Error("invalid repository identifier format, repository slug is empty")
+				return fmt.Errorf("invalid repository identifier format, repository slug is empty")
+			}
+			projectKey := parts[0]
+			slug := parts[1]
 
 			values, err := client.GetRequiredBuilds(projectKey, slug)
 			if err != nil {
@@ -38,8 +46,7 @@ func GetRequiredBuildCmd() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVarP(&projectKey, "projectKey", "k", "", "Project key (required)")
-	cmd.Flags().StringVarP(&slug, "repositorySlug", "s", "", "Repository slug (required)")
+	cmd.Flags().StringVarP(&repositorySlug, "repositorySlug", "s", "", "Repository identifier in format <projectKey>/<repositorySlug>")
 	cmd.Flags().StringVarP(&output, "output", "o", "plain", "Output format: plain|yaml|json")
 
 	return cmd
